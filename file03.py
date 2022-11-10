78a799f775a2d59aeb033f8c124be347555581db
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-11-10 18:54:06 +0500

    03

--- file03.py
+++ file03.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    list1=[]
+    for i in range(len(data)):
+        if data[i].isdigit():
+            list1.append((data[i]))
+    return list1
+
+f = open('txt_file/data03.txt').read()  
 # Read data from file
+print(main(f))
