778b9f86a373fc118987afed78a429e8ac0fe6cd
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-11-10 21:15:45 +0500

    06

--- file06.py
+++ file06.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    lenlist=[]
+    a=data.split('\n')
+    for i in a:
+        lenlist.append(len(i))
+    return lenlist
     
 # Read data from file
+f = open('txt_file/data06.txt').read()
